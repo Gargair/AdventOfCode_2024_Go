913bd4a9cb6caebec5f2295171af85967b59f483
Gargair <Gargair@users.noreply.github.com>
2025-02-10 07:45:55 +0100

    Reworked test structure

--- test/internal/solutions/Day01_test.go
+++ test/internal/solutions/Day01_test.go
@@ -3,181 +3,72 @@ package solutions_test
 import (
 	"AdventOfCode/internal/helper"
 	"AdventOfCode/internal/solutions"
-	"fmt"
-	"strconv"
-	"strings"
 	"testing"
 )
 
-type args struct {
-	inputPath     string
-	expectedLines int
-}
-
 func TestDay01_Part1(t *testing.T) {
-
-	lines, err := helper.ReadAllLines("../../testdata/Day01.txt", 2)
+	tests, err := helper.ReadTestData("../../testdata/Day01.txt", 1)
 
 	if err != nil {
 		t.Errorf("Day01: %v", err)
 		return
 	}
-	tests := make([]struct {
-		name string
-		args args
-		want string
-	}, 0, 2)
-	for _, line := range *lines {
-		res := strings.Fields(line)
-		want := res[0]
-		inputPath := res[2]
-		expectedLines, err := strconv.Atoi(res[3])
-		if err != nil {
-			t.Errorf("Day01: %v", err)
-			return
-		}
-		tests = append(tests, struct {
-			name string
-			args args
-			want string
-		}{
-			name: fmt.Sprintf("inputPath=%v/expectedLines=%v", inputPath, expectedLines),
-			args: args{
-				inputPath:     "../../../" + inputPath,
-				expectedLines: expectedLines},
-			want: want})
-	}
 	s := solutions.Day01_Solution{}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := s.Part1(tt.args.inputPath, tt.args.expectedLines); got != tt.want {
-				t.Errorf("%v = %v, want %v", tt.name, got, tt.want)
+		t.Run(tt.Name, func(t *testing.T) {
+			if got := s.Part1(tt.Args.InputPath, tt.Args.ExpectedLines); got != tt.Want {
+				t.Errorf("%v = %v, want %v", tt.Name, got, tt.Want)
 			}
 		})
 	}
 }
 
 func TestDay01_Part2(t *testing.T) {
-	lines, err := helper.ReadAllLines("../../testdata/Day01.txt", 2)
+	tests, err := helper.ReadTestData("../../testdata/Day01.txt", 2)
 
 	if err != nil {
 		t.Errorf("Day01: %v", err)
 		return
 	}
-	tests := make([]struct {
-		name string
-		args args
-		want string
-	}, 0, 2)
-	for _, line := range *lines {
-		res := strings.Fields(line)
-		want := res[1]
-		inputPath := res[2]
-		expectedLines, err := strconv.Atoi(res[3])
-		if err != nil {
-			t.Errorf("Day01: %v", err)
-			return
-		}
-		tests = append(tests, struct {
-			name string
-			args args
-			want string
-		}{
-			name: fmt.Sprintf("inputPath=%v/expectedLines=%v", inputPath, expectedLines),
-			args: args{
-				inputPath:     "../../../" + inputPath,
-				expectedLines: expectedLines},
-			want: want})
-	}
 	s := solutions.Day01_Solution{}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := s.Part2(tt.args.inputPath, tt.args.expectedLines); got != tt.want {
-				t.Errorf("%v = %v, want %v", tt.name, got, tt.want)
+		t.Run(tt.Name, func(t *testing.T) {
+			if got := s.Part2(tt.Args.InputPath, tt.Args.ExpectedLines); got != tt.Want {
+				t.Errorf("%v = %v, want %v", tt.Name, got, tt.Want)
 			}
 		})
 	}
 }
 
 func BenchmarkDay01_Part1(b *testing.B) {
-	lines, err := helper.ReadAllLines("../../testdata/Day01.txt", 2)
+	tests, err := helper.ReadTestData("../../testdata/Day01.txt", 1)
 
 	if err != nil {
 		b.Errorf("Day01: %v", err)
 		return
 	}
-	tests := make([]struct {
-		name string
-		args args
-		want string
-	}, 0, 2)
-	for _, line := range *lines {
-		res := strings.Fields(line)
-		want := res[0]
-		inputPath := res[2]
-		expectedLines, err := strconv.Atoi(res[3])
-		if err != nil {
-			b.Errorf("Day01: %v", err)
-			return
-		}
-		tests = append(tests, struct {
-			name string
-			args args
-			want string
-		}{
-			name: fmt.Sprintf("inputPath=%v/expectedLines=%v", inputPath, expectedLines),
-			args: args{
-				inputPath:     "../../../" + inputPath,
-				expectedLines: expectedLines},
-			want: want})
-	}
 	s := solutions.Day01_Solution{}
 	for _, tt := range tests {
-		b.Run(tt.name, func(b *testing.B) {
+		b.Run(tt.Name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				s.Part1(tt.args.inputPath, tt.args.expectedLines)
+				s.Part1(tt.Args.InputPath, tt.Args.ExpectedLines)
 			}
 		})
 	}
 }
 
 func BenchmarkDay01_Part2(b *testing.B) {
-	lines, err := helper.ReadAllLines("../../testdata/Day01.txt", 2)
+	tests, err := helper.ReadTestData("../../testdata/Day01.txt", 2)
 
 	if err != nil {
 		b.Errorf("Day01: %v", err)
 		return
 	}
-	tests := make([]struct {
-		name string
-		args args
-		want string
-	}, 0, 2)
-	for _, line := range *lines {
-		res := strings.Fields(line)
-		want := res[1]
-		inputPath := res[2]
-		expectedLines, err := strconv.Atoi(res[3])
-		if err != nil {
-			b.Errorf("Day01: %v", err)
-			return
-		}
-		tests = append(tests, struct {
-			name string
-			args args
-			want string
-		}{
-			name: fmt.Sprintf("inputPath=%v/expectedLines=%v", inputPath, expectedLines),
-			args: args{
-				inputPath:     "../../../" + inputPath,
-				expectedLines: expectedLines},
-			want: want})
-	}
 	s := solutions.Day01_Solution{}
 	for _, tt := range tests {
-		b.Run(tt.name, func(b *testing.B) {
+		b.Run(tt.Name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				s.Part2(tt.args.inputPath, tt.args.expectedLines)
+				s.Part2(tt.Args.InputPath, tt.Args.ExpectedLines)
 			}
 		})
 	}
